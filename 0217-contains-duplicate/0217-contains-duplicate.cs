5b51ef772900263452fa4fae87f8693383c79e38
Momchil Yankov <47501287+Momchil2k17@users.noreply.github.com>
2025-02-26 00:40:37 +0200

    Time: 11 ms (83.33%), Space: 67.1 MB (42.19%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cs
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -1,6 +1,15 @@
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
-        var hashNum = new HashSet<int>(nums);
-        return hashNum.Count<nums.Length;
+        var hashNum = new HashSet<int>();
+        foreach (int i in nums) 
+        {   
+            int count=hashNum.Count;
+            hashNum.Add(i);
+            if (count==hashNum.Count)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
